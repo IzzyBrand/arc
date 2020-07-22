16c90881c7a41fb8a1f0feaa25779ae76e723a63
Izzy Brand <izzybrand@gmail.com>
2020-07-22 11:55:47 -0700

    add script to experiment with loading task

---
+++ test_load_task.py
@@ -0,0 +1,29 @@
+import json
+from matplotlib import pyplot as plt
+from matplotlib.colors import ListedColormap
+import numpy as np
+
+with open('ARC/data/training/469497ad.json') as f: 
+    j = json.load(f) 
+
+print(j['train'][0])
+print(j['test'][0])
+
+# I grabbed this colormap from the CSS on the ARC website.
+cmap = ListedColormap([
+    "#000000",
+    "#0074D9",
+    "#FF4136",
+    "#2ECC40",
+    "#FFDC00",
+    "#AAAAAA",
+    "#F012BE",
+    "#FF851B",
+    "#7FDBFF",
+    "#870C25"])
+
+plt.imshow(j['train'][0]['input'], cmap=cmap, interpolation='nearest')
+plt.show()
+
+
+

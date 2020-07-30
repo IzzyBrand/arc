ed1945d2d004c79821c80563e7b022ae85c9986a
Izzy Brand <izzybrand@gmail.com>
2020-07-30 15:54:59 -0700

    add a few more primitives including identities

--- primitives.py
+++ primitives.py
@@ -35,27 +35,50 @@ def Color_p(i):
 # Primitive Definitions
 ###############################################################################
 
+# these four primitives are the identity
+def PassGrid(args):
+    return args
+create_primitive(PassGrid, 'Grid', 'Grid')
+
+def PassInt(args):
+    return args
+create_primitive(PassInt, 'Int', 'Int')
+
+def PassMask(args):
+    return args
+create_primitive(PassMask, 'Mask', 'Mask')
+
+def PassColor(args):
+    return args
+create_primitive(PassColor, 'Color', 'Color')
+
+# primtiives which operate on grids, masks and shapes
 def VFlip(args):
     grid, = args
     return np.flip(grid, axis=0)
+create_primitive(VFlip, 'Grid', 'Grid')
 
 def HFlip(args):
     grid, = args
     return np.flip(grid, axis=1)
+create_primitive(HFlip, 'Grid', 'Grid')
 
-def GridEqual(args):
+def EqualGrid(args):
     grid1, grid2 = args
     return grid1 == grid2
+create_primitive(EqualGrid, ['Grid', 'Grid'], 'Mask')
 
-def ColorMask(args):
-    grid, color = args
-    return grid == color
+def InvertMask(args):
+    mask, = args
+    return 1 - mask
+create_primitive(InvertMask, 'Mask', 'Mask')
 
-def ColorCount(args):
-    grid, color = args
-    return (grid == color).sum()
+def Tile(args):
+    grid, x, y
+    return np.tile(grid, (y, x))
+create_primitive(Tile, ['Grid', 'Int', 'Int'], 'Grid')
 
-def PatchExtract(args):
+def ExtractPatch(args):
     grid, x, y, w, h = args
     # TODO(izzy): at some point we'll have to be smarter about selecting
     # indices and dimensions. for the minute, we'll just pretend the function
@@ -64,19 +87,34 @@ def PatchExtract(args):
         return grid[y:y+h, x:x+w]
     except IndexError:
         return grid
+create_primitive(ExtractPatch, ['Grid', 'Int', 'Int', 'Int', 'Int'], 'Grid')
 
-def PatchCreate(args):
+def CreatePatch(args):
     w, h, color = args
     return np.ones([h,w]) * color
+create_primitive(CreatePatch, ['Int', 'Int', 'Color'], 'Grid')
 
-create_primitive(VFlip, 'Grid', 'Grid')
-create_primitive(HFlip, 'Grid', 'Grid')
-create_primitive(GridEqual, ['Grid', 'Grid'], 'Mask')
+def ColorMask(args):
+    grid, color = args
+    return grid == np.ones_like(grid)*color
 create_primitive(ColorMask, ['Grid', 'Color'], 'Mask')
-create_primitive(ColorCount, ['Grid', 'Color'], 'Int')
-create_primitive(PatchExtract, ['Grid', 'Int', 'Int', 'Int', 'Int'], 'Grid')
-create_primitive(PatchCreate, ['Int', 'Int', 'Color'], 'Grid')
+
+def CountColor(args):
+    grid, color = args
+    return (grid == color).sum()
+create_primitive(CountColor, ['Grid', 'Color'], 'Int')
+
+def Contract(args):
+    grid, mask = args
+    h, w = mask.shape
+    v_idxs = np.arange(h)[mask.sum(axis=1) > 0]
+    h_idxs = np.arange(w)[mask.sum(axis=0) > 0]
+    v_min, v_max = np.min(v_idxs), np.max(v_idxs)
+    h_min, h_max = np.min(h_idxs), np.max(h_idxs)
+    return grid[v_min:v_max+1, h_min:h_max+1]
+create_primitive(Contract, ['Grid', 'Mask'], 'Grid')
 
 
 if __name__ == '__main__':
-    print([p.name for p in language])
+    for p in language:
+        print(f'{p.name}:\t{p.input_type} -> {p.output_type}')

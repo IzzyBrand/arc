fd9c8b11a552d2ffc0e87a8bbb0d6f4aa15b56ac
Izzy Brand <izzybrand@gmail.com>
2020-10-22 08:35:28 -0400

    change tabs to spaces

--- arc_lisp_env.py
+++ arc_lisp_env.py
@@ -5,31 +5,30 @@ Izzy Brand, 2020
 import numpy as np
 
 def array_assign(*args):
-	A = np.copy(args[0])
-	A[tuple(args[1:-1])] = args[-1]
-	return A
+    A = np.copy(args[0])
+    A[tuple(args[1:-1])] = args[-1]
+    return A
 
-def make_grid(task_name, index = 0, subset = 'train'):
-	try:
-		with open(f'/ARC/data/training/{task_name}.json') as f:
-			j = json.load(f)
+def make_grid(task_name, index=0, subset='train'):
+    try:
+        with open(f'/ARC/data/training/{task_name}.json') as f:
+            j = json.load(f)
 
-		return j[subset][index]['input']
-	except:
-		print("TypeError: invalid parameters for make_grid()")
+        return j[subset][index]['input']
+    except:
+        print("TypeError: invalid parameters for make_grid()")
 
 extended_env = {
-	'zeros_like': np.zeros_like,
-	'logical_and': np.logical_and,
-	'ndarray': lambda *x: np.ndarray(list(x)),
-	'where': np.where,
-	'array_assign': array_assign,
-	':': slice(None),
-	'slice': slice,
-	'None': None,
-	'map': map,
-	'max': max,
-	'set': set,
-	'make_grid': make_grid,
-
+    'zeros_like': np.zeros_like,
+    'logical_and': np.logical_and,
+    'ndarray': lambda *x: np.ndarray(list(x)),
+    'where': np.where,
+    'array_assign': array_assign,
+    ':': slice(None),
+    'slice': slice,
+    'None': None,
+    'map': map,
+    'max': max,
+    'set': set,
+    'make_grid': make_grid,
 }

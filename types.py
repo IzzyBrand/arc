79c2e99167e87aed2bde1fa1c6b8e83e8a3cdfdc
Izzy Brand <izzybrand@gmail.com>
2020-10-31 13:07:01 -0400

    add templated types

--- types.py
+++ types.py
@@ -1,32 +1,84 @@
-class Type(list):
-    def __init__(self, *args):
-        super(Type, self).__init__(args)
+class Type:
+    """ Base type for things like numbers and arrays
+    """
+    def __init__(self, T):
+        self.T = T
 
     def __str__(self):
-        if len(self) == 1:
-            return str(self[0])
-        else:
-            return ', '.join(str(t) for t in self)
+        return self.T
+
+class TupleType(Type):
+    """ Describes the type of a tuple of items
+
+    Extends:
+        Type
+    """
+    def __init__(self, *args):
+        super(TupleType, self).__init__(args)
+
+    def __str__(self):
+        return f"({', '.join(str(t) for t in self.T)})"
+
+class ArrayType(Type):
+    """ Describes the type of an array or list of items.
+
+    Extends:
+        Type
+    """
+    def __init__(self, T):
+        super(ArrayType, self).__init__(T)
+
+    def __str__(self):
+        return f"ArrayOf{str(self.T)}"
 
 class FuncType(Type):
+    """ Describes the type of a function. Functions with multiple inputs or
+    multiples outputs use the TupleType to wrap the inputs and outputs
+
+    Extends:
+        Type
+    """
+    def __init__(self, T_in, T_out):
+
+        # convert types to tuples as needed
+        if isinstance(T_in, tuple): T_in = TupleType(*T_in)
+        if isinstance(T_out, tuple): T_out = TupleType(*T_out)
+
+        super(FuncType, self).__init__((T_in, T_out))
+
     def __str__(self):
-        input_str = str(self[0])
-        if len(self[0]) > 1: 
-            input_str = f'({input_str})'
+        return f"{str(self.T[0])} -> {str(self.T[1])}"
 
-        output_str = str(self[1])
-        if len(self[1]) > 1: 
-            output_str = f'({output_str})'
+class TemplateType(Type):
+    """ Some functions can handle multiple types. For example map has a return
+    type which depends on the function being mapped. This template type is for
+    that purpose
 
-        return f"{input_str} -> {output_str}"
+    Extends:
+        Type
+    """
+    def __init__(self, T):
+        super(TemplateType, self).__init__(T)
+
+    def __str__(self):
+        return f"<{self.T}>"
 
 if __name__ == '__main__':
     i_T = Type("Int")
     c_T = Type("Color")
     a_T = Type("Array")
+    l_t = Type("List")
 
-    # example of a function that counts the number of pixels of
-    # a certain color
-    colorcount_T = FuncType(Type(a_T, c_T), Type(i_T))
+    # example of a function that counts the number of pixels of a certain color
+    colorcount_T = FuncType((a_T, c_T), i_T)
+    print("colorcount:", colorcount_T)
 
-    print(colorcount_T)
+    # example of the type of map using a template type
+    T1 = TemplateType(1)
+    T2 = TemplateType(2)
+    input_type = (ArrayType(T1), FuncType(T1, T2))
+    output_type = ArrayType(T2)
+    map_T = FuncType(input_type, output_type)
+    print("map:", map_T)
+
+
